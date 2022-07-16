ccf430f3c1fb11e0cadde793ec136907edc4b783
shiv-pratap2002 <83550179+shiv-pratap2002@users.noreply.github.com>
2022-07-16 15:15:59 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.21%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num<10)
+        {
+            return num;
+        }
+        else
+        {
+            return addDigits(num%10+num/10);
+        }
+    }
+};
