06ddb9ddef098a8602b6aa6e0296d2a2ae005379
shiv-pratap2002 <83550179+shiv-pratap2002@users.noreply.github.com>
2022-08-09 17:38:13 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (44.70%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -22,48 +22,7 @@ public:
             return 1+max(height(p->left),height(p->right));
         }
     }
-    vector<int> lvl_order(TreeNode *r)
-    {
-    vector<int>c(height(r),0);
-    if(r==NULL)
-    {
-        return c;
-    }
-    queue<TreeNode *>q;
-    q.push(r);    
-    c.push_back(r->val);
-    while(true)
-    {
-        int n=q.size();
-        if(n==0)
-        {
-            return c;
-        }
-        while(n>0)
-        {
-            TreeNode *p=q.front();
-            q.pop();
-            if(p->left==nullptr or p->right==nullptr)
-            {
-                c.push_back(-1);
-            }
-            if(p->left!=NULL)
-            {
-                q.push(p->left);
-                c.push_back((p->left)->val);
-
-            }
-            if(p->right!=NULL)
-            {
-                q.push(p->right);
-                c.push_back((p->right)->val);
-            }
-            n--;
-        }
-        
-    }
     
-}
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(height(p)!=height(q))
         {
@@ -81,22 +40,5 @@ public:
         {
             return isSameTree(p->left,q->left) and isSameTree(p->right,q->right);
         }
-        // vector<int>l = lvl_order(p);
-        // vector<int>d = lvl_order(q);
-        // if(l.size()!=d.size())
-        // {
-        //     return false;
-        // }
-        // else
-        // {
-        //     for(int i=0;i<l.size();i++)
-        //     {
-        //         if(l[i]!=d[i])
-        //         {
-        //             return false;
-        //         }
-        //     }
-        //     return true;
-        // }
     }
 };
